9baae0339a65d06a25012d225b42761872313214
Arthur <98979121+Arthur-lfm@users.noreply.github.com>
2023-03-26 19:21:57 +0100

    Update settings.py

--- pollme/settings.py
+++ pollme/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pollme.middleware.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'pollme.urls'
